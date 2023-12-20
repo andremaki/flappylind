80ff2be33736ec4596306d39fd9b92770190cb67
Andre <andre.kunnapas.maki-viinikka@ut.ee>
2023-12-20 17:56:07 +0200

    s

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ import random
 
 pygame.init()
 
-SCREEN_WIDTH = 600
+SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
+
+lind_image = pygame.image.load('pildid/lennuk.png')
+lind_rect = lind_image.get_rect()
+
+pipe_image = pygame.image.load('pildid/image-removebg-preview.png')
 
 lind_x = 50
 lind_y = 300
-lind_width = 40
-lind_height = 40
+lind_width = 100
+lind_height = 100
 gravity = 0.5
 lind_velocity = 0
 
+lind_image = pygame.transform.scale(lind_image, (lind_width, lind_height))
+
 pipe_width = 70
-pipe_height = random.randint(150, 450)
 pipe_color = (0, 255, 0)
-pipe_x = 400
 space_between_pipes = 200
 
 score = 0
@@ -26,6 +31,26 @@ pygame.display.set_caption("Flappy Lind")
 
 running = True
 clock = pygame.time.Clock()
+
+passed_pipe = False
+
+class Pipe:
+    def __init__(self, x, height):
+        self.x = x
+        self.height = height
+        self.width = pipe_width
+        self.image_upper = pygame.transform.scale(pipe_image, (pipe_width, height))
+        self.image_lower = pygame.transform.scale(pipe_image, (pipe_width, SCREEN_HEIGHT - height - space_between_pipes))
+        self.rect_upper = pygame.Rect(x, 0, pipe_width, height)
+        self.rect_lower = pygame.Rect(x, height + space_between_pipes, pipe_width, SCREEN_HEIGHT - height - space_between_pipes)
+
+
+pipes = []
+initial_pipe_count = 6
+pipe_spacing = 300
+for i in range(initial_pipe_count):
+    new_pipe = Pipe(400 + i * pipe_spacing, random.randint(150, 450))
+    pipes.append(new_pipe)
 
 while running:
     screen.fill((0, 0, 0))
@@ -39,24 +64,35 @@ while running:
 
     lind_velocity += gravity
     lind_y += lind_velocity
+    lind_rect.topleft = (lind_x, lind_y)
 
-    pipe_x -= 2
-    if pipe_x < -pipe_width:
-        pipe_x = SCREEN_WIDTH
-        pipe_height = random.randint(150, 450)
-        score += 1
+    for pipe in pipes:
+        pipe.x -= 4
 
-    pygame.draw.rect(screen, (255, 255, 0), (lind_x, lind_y, lind_width, lind_height))
+    if pipes[0].x < -pipe_width:
+        pipes.pop(0)
+        new_pipe = Pipe(pipes[-1].x + pipe_spacing, random.randint(150, 450))
+        pipes.append(new_pipe)
+        passed_pipe = False
 
-    pygame.draw.rect(screen, pipe_color, (pipe_x, 0, pipe_width, pipe_height))
-    pygame.draw.rect(screen, pipe_color, (pipe_x, pipe_height + space_between_pipes, pipe_width, SCREEN_HEIGHT))
+    for pipe in pipes:
+        if not passed_pipe and lind_x > pipe.x + pipe.width:
+            passed_pipe = True
+            score += 1
+
+    for pipe in pipes:
+        screen.blit(pipe.image_upper, (pipe.x, 0))
+        screen.blit(pipe.image_lower, (pipe.x, pipe.height + space_between_pipes))
 
     if lind_y > SCREEN_HEIGHT - lind_height or lind_y < 0:
         running = False
 
-    if pipe_x < lind_x + lind_width and lind_x < pipe_x + pipe_width:
-        if lind_y < pipe_height or lind_y > pipe_height + space_between_pipes:
-            running = False
+    for pipe in pipes:
+        if lind_x + lind_width > pipe.x and lind_x < pipe.x + pipe.width:
+            if lind_y < pipe.height or lind_y + lind_height > pipe.height + space_between_pipes:
+                running = False
+
+    screen.blit(lind_image, (lind_x, lind_y))
 
     font = pygame.font.SysFont(None, 36)
     text = font.render(f"Score: {score}", True, (255, 255, 255))
